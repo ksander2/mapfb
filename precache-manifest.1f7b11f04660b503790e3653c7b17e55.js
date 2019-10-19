f56283a7363099cf72a204eaaada86e5659c479f
ksander2 <an_sobolev90@mail.ru>
2019-10-19 12:10:33 +0700

    Updates

---
+++ precache-manifest.1f7b11f04660b503790e3653c7b17e55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a1af67659a4839da255276964bdbd28",
+    "url": "/mapfb/index.html"
+  },
+  {
+    "revision": "5bc50bed8f3c836718bc",
+    "url": "/mapfb/static/css/main.91ecf581.chunk.css"
+  },
+  {
+    "revision": "607082609def06f08065",
+    "url": "/mapfb/static/js/2.71c91af3.chunk.js"
+  },
+  {
+    "revision": "5bc50bed8f3c836718bc",
+    "url": "/mapfb/static/js/main.0fab49a0.chunk.js"
+  },
+  {
+    "revision": "cf1af7012b27e3a2232f",
+    "url": "/mapfb/static/js/runtime-main.794948bd.js"
+  }
+]);
